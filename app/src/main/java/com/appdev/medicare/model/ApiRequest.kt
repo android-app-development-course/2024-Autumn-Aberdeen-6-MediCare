b5b63f4b2c4192ce8e7a0544fb1b779fc5c381c7
Leo204-LKY <leo2004526@foxmail.com>
2024-12-18 13:54:58 +0800

    移除弃用的 API Request Body

--- app/src/main/java/com/appdev/medicare/model/ApiRequest.kt
+++ app/src/main/java/com/appdev/medicare/model/ApiRequest.kt
@@ -10,41 +10,6 @@ data class LoginRequest(
     val passwordHash: String
 )
 
-data class AddMedicationRequest(
-    val medicationName: String,
-    val patientName: String,
-    val dosage: String,
-    val remainingAmount: Int,
-    val frequency: String,
-    val weekMode: String,
-    val reminderType: String,
-    val expirationDate: String,
-    val dateList: List<String>,
-    val timeList: List<String>
-)
-
-data class GetMedicationTimesRequest(
-    val medicationId: Int,
-    val date: String
-)
-
-data class GetMedicationRecordsRequest(
-    val date: String
-)
-
-data class GetMedicationInfoRequest(
-    val medicationId: Int
-)
-
-data class GetAllOnDateRequest(
-    val date: String
-)
-
-data class DeleteMedicationRecordRequest(
-    val date: String,
-    val medicationId: Int
-)
-
 data class InsertMedicationDataRequest(
     val medicationTime: List<Map<String, JsonValue>>
 )
