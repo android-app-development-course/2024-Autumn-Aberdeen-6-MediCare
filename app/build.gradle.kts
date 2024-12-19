fd430e5777a4ba5df55e7c6eed7a0d74cea3004a
yc1016 <2449622446@qq.com>
2024-12-19 15:18:58 +0800

    小调整

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.appdev.medicare"
-        minSdk = 31
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
